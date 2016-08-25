faf3d79a1563325973bf0bfb1b0e38ed22a0e7eb
strawgate <williamseaston@gmail.com>
2016-08-25 13:12:15 -0500

    Updating Client Health Script

--- Helpers/BigFix Client Health/ClientHealth.ps1
+++ Helpers/BigFix Client Health/ClientHealth.ps1
@@ -1,163 +1,200 @@
 param (
-    [string] $Masthead,
+    [string] $Masthead = "http://bigfix:52311/masthead/masthead.afxm",
     [string] $Installer = "http://software.bigfix.com/download/bes/95/BigFix-BES-Client-9.5.2.56.exe"
 )
 
-Function Check-ClientExe {
-    param (
-        $InstallDir
-    )
-    if (!(test-path $InstallDir)) {throw "BigFix Installation directory missing"}
-    if (!(test-path "$InstallDir\besclient.exe")) {throw "BigFix executable missing"}
-}
+$RegistrySoftwareRoot = if ($ENV:PROCESSOR_ARCHITECTURE -eq "AMD64") { "HKLM:\Software\WOW6432Node" } else { "HKLM:\Software" }
+$ProgramFilesRoot = if ($ENV:PROCESSOR_ARCHITECTURE -eq "AMD64") { ${env:ProgramFiles(x86)} } else { ${env:ProgramFiles} }
 
-Function Check-ClientLogs {
+function Check-RegistryKeys {
     param (
-        $InstallDir
+        [string[]] $Keys
     )
-    if (!(test-path $InstallDir)) {throw "BigFix Installation directory missing"}
-    if (!(test-path "$InstallDir\__BESData")) {throw "BigFix __BESData directory missing"}
-    if (!(test-path "$InstallDir\__BESData\__Global")) {throw "BigFix __BESData\Global directory missing"}
-    if (!(test-path "$InstallDir\__BESData\__Global\Logs")) {throw "BigFix __BESData\Global\Logs directory missing"}
 
-    $Logs = get-childitem -path "$InstallDir\__BESData\__Global\Logs"
-    if (!($Logs | where-object {$_.LastWriteTime.ToShortDateString() -eq (Get-Date).ToShortDateString()})) { 
-        write-log "No BigFix Log for Today"
-        if (!($Logs | where-object {$_.LastWriteTime.ToShortDateString() -eq (Get-Date).addDays(-1).ToShortDateString()})) {     
-            throw "No BigFix Log for yesterday"
+    foreach ($Key in $Keys) {
+        if (!(test-path $Key)) {
+            throw "$Key does not exist"
         }
     }
-
-    if (!(test-path "$InstallDir\besclient.exe")) {throw "BigFix executable missing"}
 }
 
-Function Check-ClientRegistry {
-    if(!(Get-InstallDir)) { throw "Unable to locate BigFix in the Windows Registry" }
+function Check-RegistryValues {
+    param (
+        [string[][]] $KeyValuePairs
+    )
+
+    foreach ($Pair in $KeyValuePairs) {
+        $Key = $Pair[0]
+        $Value = $Pair[1]
+        try {
+            (Get-ItemProperty -Path $Key)."$Value" | out-null
+        } catch {
+            throw "$Value not in $Key"
+        }
+    }
 }
 
-Function Get-InstallDir {
-    if ($ENV:PROCESSOR_ARCHITECTURE -eq "AMD64") {
-        $Registry = test-path "HKLM:\SOFTWARE\WOW6432Node\BigFix"
-        $InstallDir = (Get-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\BigFix\EnterpriseClient" -ErrorAction SilentlyContinue).EnterpriseClientFolder
-    } else {
-        $Registry = test-path "HKLM:\SOFTWARE\BigFix"
-        $InstallDir = (Get-ItemProperty -Path "HKLM:\SOFTWARE\BigFix\EnterpriseClient" -ErrorAction SilentlyContinue).EnterpriseClientFolder
+function Check-Directories {
+    param (
+        [string[]] $Directories
+    )
+    foreach ($Directory in $Directories) {
+        if (!(resolve-path $Directory)) {
+            throw "$Directory does not exist"
+        }
+    }
+}
+
+function Check-Files {
+    param (
+        [string[]] $Files
+    )
+    foreach ($File in $Files) {
+        if (!(resolve-path $File)) {
+            throw "$File does not exist"
+        }
+    }
+}
+
+function Check-Services {
+    param (
+        [string[]] $Services,
+        [switch] $Repair
+    )
+
+    foreach ($Service in $Services) {
+        $Service = get-service $Service -ErrorAction SilentlyContinue
+
+        if (!($Service)) { throw "$Service does not exist" }
+
+        if ($Repair) {
+            if ($Service.Status -ne "Running") {
+                $Service | Start-Service
+                Start-Sleep -Seconds 30
+            }
+
+            if ($Service.StartType -ne "Automatic") {
+                $Service | set-service -StartupType Automatic
+            }
+        }
+    }
+}
+
+Function Invoke-Repair {
+    param (
+        [string[][]]$Actions,
+        $Cache
+    )
+    foreach ($ActionPair in $Actions) {
+        $Exe = $ActionPair[0]
+        $Cmdline = $ActionPair[1]
+        start-process -FilePath $Exe -ArgumentList $Cmdline -WorkingDirectory $Cache -wait
     }
 
-    return $InstallDir
 }
 
-Function Check-Service {
-    $Service = get-service besclient -ErrorAction SilentlyContinue
+Function Invoke-Downloads {
+    param (
+        $Downloads,
+        $Cache
+    )
+    new-item $Cache -ItemType directory -ErrorAction SilentlyContinue
 
-    if (!($Service)) { return $False }
+    foreach ($Download in $Downloads.GetEnumerator()) {
+        $wc = new-object System.Net.WebClient
 
-    # Check Service State
-    if ($Service.Status -ne "Running") {
-        write-log "Starting Service"
-        $Service | Start-Service
-        $Service | set-service -StartupType Automatic
+        $Destination = "$Cache\$($Download.Name)"
+
+        remove-item $Destination -ErrorAction SilentlyContinue
+    
+        $wc.DownloadFile($Download.Value, $Destination)
+
+        $wc.Dispose()
     }
-
-    return $Service
-}
-
-Function Backup-BESData {
-    write-log -Text "Backing up __BESData" -ID "1338"
-
-    $InstallDir = Get-InstallDir
-    if(!($InstallDir)) {write-log "Nothing to backup"; return}
-
-    $BackupDir = "$InstallDir\__BESData" -replace("BES Client","__BESData.bak")
-
-    move-item "$InstallDir\__BESData" "$($env:windir)\BigFix\__BESData-$(get-date -format "yyyyMMdd-HHmmss").bak"
-}
-
-Function Clean-BigFix {
-    write-log -Text "Cleaning off BigFix" -ID "1338"
-    
-    start-process "$($env:windir)\BigFix\Installer Cache\BES-Clean.exe" -ArgumentList "/silent /client /force" -wait
-}
-
-Function Cache-Downloads {
-
-    new-item "$($env:windir)\BigFix\Installer Cache" -ItemType directory -ErrorAction SilentlyContinue
-    $wc = new-object System.Net.WebClient
-
-    #Cleaner
-    write-log -Text "Downloading Cleaner" -ID "1338"
-
-    $Source = "https://www.ibm.com/developerworks/community/wikis/form/anonymous/api/wiki/90553c0b-42eb-4df0-9556-d3c2e0ac4c52/page/90bfd4e5-98b9-4a9b-a6cb-812f1f8d5702/attachment/63b54d70-2290-4044-8b67-6eb0a4d66cfc/media/BESRemove9.5.0.311.exe"
-    $Destination = "$($env:windir)\BigFix\Installer Cache\BES-Clean.exe"
-    remove-item "$Destination" -ErrorAction SilentlyContinue
-    
-    $wc.DownloadFile($source, $destination)
-
-    # Setup
-    write-log -Text "Downloading BigFix Setup"  -ID "1338"
-    
-    $Source = $Installer
-    $Destination = "$($env:windir)\BigFix\Installer Cache\BES-Setup.exe"
-    
-    remove-item "$Destination" -ErrorAction SilentlyContinue
-    $wc.DownloadFile($source, $destination)
-    if (!(test-path "$Destination")) { throw "Error Downloading Installer" }
-
-    #Masthead
-    write-log "Downloading MastHead" -ID "1338"
-    
-    $Source = $Masthead
-    $Destination = "$($env:windir)\BigFix\Installer Cache\masthead.afxm"
-    
-    remove-item "$Destination" -ErrorAction SilentlyContinue
-    
-    $wc.DownloadFile($source, $destination)
-    if (!(test-path "$Destination")) { throw "Error Downloading Installer" }
-}
-
-Function Install-BigFix {
-
-    write-log "Installing BigFix"
-
-    start-process "$($env:windir)\BigFix\Installer Cache\BES-Setup.exe" -ArgumentList "/s /v/qn" -wait
 }
 
 function write-log {
     param (
         $Text,
-        $ID = 1337
+        [ValidateSet(“Verbose”,”Error”,”Summary”)] 
+        $Type = "Verbose"
     )
+    if ($Type -like "Verbose") { $ID = 1337; $EntryType = "Information" }
+    if ($Type -like "Error")   { $ID = 1338; $EntryType = "Error" }
+    if ($Type -like "Summary") { $ID = 1340; $EntryType = "SuccessAudit" }
 
     New-EventLog -LogName "BigFix Client Health" -Source "Client Health" -ErrorAction SilentlyContinue
     Limit-EventLog -LogName "BigFix Client Health" -Retention 28 -ErrorAction SilentlyContinue
-
+    
     write-host $Text
-    write-eventlog -logname "BigFix Client Health" -source "Client Health" -eventID $ID -entrytype Information -message "$Text"
+    write-eventlog -logname "BigFix Client Health" -source "Client Health" -eventID $ID -EntryType $EntryType -message "$Text"
 }
 
 try {
-    Check-Service
-    Get-InstallDir
-    throw "test"
-    Check-ClientRegistry
-    Check-ClientExe -InstallDir (Get-InstallDir)
-    Check-ClientLogs -InstallDir (Get-InstallDir)
-    write-log "BigFix Client Health Check Passed"
+    $Services = @(
+        "BESClient"
+    )
+
+    Check-Services $Services -Repair
+
+    $RegistryKeys = @(
+        @(
+            "$RegistrySoftwareRoot\BigFix",
+            "$RegistrySoftwareRoot\BigFix\EnterpriseClient"
+        )
+    )
+
+    $RegistryValues = @(
+        @("$RegistrySoftwareRoot\BigFix\EnterpriseClient", "EnterpriseClientFolder"),
+        @("$RegistrySoftwareRoot\BigFix\EnterpriseClient\GlobalOptions", "ComputerId")
+    )
+
+    Check-RegistryKeys $RegistryKeys
+    Check-RegistryValues $RegistryValues
+
+    $InstallDir = (Get-ItemProperty -Path "$RegistrySoftwareRoot\BigFix\EnterpriseClient").EnterpriseClientFolder
+
+    $Directories = @(
+        "$InstallDir",
+        "$InstallDir\__BESData",
+        "$InstallDir\__BESData\__Global",
+        "$InstallDir\__BESData\__Global\Logs",
+        "$InstallDir\__BESData\actionsite"
+    )
+
+    $Files = @(
+        "$InstallDir\besclient.exe",
+        "$InstallDir\__BESData\actionsite\ActionSite.afxm"
+    )
+
+    #If it's after 6am do a log check too
+    if ((get-date).hour -gt 6) {$Files += "$InstallDir\__BESData\__Global\Logs\$((Get-Date).tostring("yyyyMMdd")).log"}
+    
+    Check-Directories $Directories
+    Check-Files $Files
+
+    write-log "Client Health Check Passed" -Type Summary
+
 } catch {
+
     write-log -Text $_.Exception.Message -ID "1340"
     
-    Cache-Downloads
+    $Downloads = @{
+        "Cleaner.exe" = "https://www.ibm.com/developerworks/community/wikis/form/anonymous/api/wiki/90553c0b-42eb-4df0-9556-d3c2e0ac4c52/page/90bfd4e5-98b9-4a9b-a6cb-812f1f8d5702/attachment/63b54d70-2290-4044-8b67-6eb0a4d66cfc/media/BESRemove9.5.0.311.exe";
+        "Client.exe" = $Installer;
+        "masthead.afxm" = $MastHead;
+    }
 
-    write-log -Text "Stopping BESClient Service" -ID "1338"
-    Stop-service -Name BESClient -ErrorAction SilentlyContinue -Force
+    $Repair = @(
+        @("net.exe", "stop besclient"),
+        @("Robocopy.exe", "/E ""$InstallDir\__BESData"" ""$($env:windir)\BigFix\Backup\$(get-date -format "yyyyMMdd-HHmmss")"""),
+        @("Cleaner.exe", "/silent /client /force"),
+        @("Client.exe", "/s /v/qn")
+    )
 
-    start-sleep -Seconds 5
-    
-    Backup-BESData
-    
-    Clean-BigFix
-    
-    Install-BigFix
+    Invoke-Downloads -Downloads $Downloads -Cache "$($env:windir)\BigFix\Cache"
 
-    write-log -Text "Done with Client Repair" -ID "1339"
+    Invoke-Repair -Actions $Repair -Cache "$($env:windir)\BigFix\Cache"
+
+    write-log "Client Repair Completed" -Type Error
 }
